483e146289e8c2b79969a6bfb8354f1a87eb5545
Eithan Refaely <eithanrefaely@gmail.com>
2021-10-18 01:29:26 +0300

    Submitting

---
+++ modules 06/calc/app/helpers/math.js
@@ -0,0 +1,20 @@
+export const add = (n1, n2) => {
+    return n1 + n2;
+}
+
+export const sub = (n1, n2) => {
+    return n1 - n2;
+}
+
+export const multiply = (n1, n2) => {
+    return n1 * n2;
+}
+
+export const divide = (n1, n2) => {
+    return n1/n2;
+}
+
+export const equals = (n1, n2, callback) => {
+   return callback(n1, n2);
+}
+
